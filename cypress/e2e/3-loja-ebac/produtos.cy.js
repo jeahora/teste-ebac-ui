255b8d21a98ab6037ceba8c2a43f9531197c4e00
Jeane Hora <jeane.souza.hora@gmail.com>
2024-06-27 21:20:24 +0100

    produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: produtos', () => {
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+});
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(0)
+            .contains('Ajax Full-Zip Sweatshirt')
+            .click()
+
+
+            cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+
+    });
+
+
+});
