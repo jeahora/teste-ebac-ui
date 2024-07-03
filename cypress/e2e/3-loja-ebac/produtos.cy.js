1a2992cff338487cb3744a8403ec5224dea19aef
Jeane Hora <jeane.souza.hora@gmail.com>
2024-07-03 21:22:35 +0100

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,22 +1,46 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: produtos', () => {
 
 beforeEach(() => {
-    cy.visit('produtos')
+    produtosPage.visitarUrl()
 });
+
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(0)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
-
-
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
             cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
 
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+       produtosPage.visitarProduto('Zeppelin Yoga Pant')
+       cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant') 
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
     });
 
 
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho,
+                dados[2].cor,
+                dados[2].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
+
+        
+    });
 });
